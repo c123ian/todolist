b8b2285951bbb7ae0b5981dad74d9804235f67d7
HammerMorty <c123ian@gmail.com>
2017-11-15 15:09:46 +0000

    Add files via upload
    added files

---
+++ api/routes/todoListRoutes.js
@@ -0,0 +1,17 @@
+/// setting up Routes: how an application responds to a client request for a specific endpoint
+
+'use strict';
+module.exports = function(app) {
+  var todoList = require('../controllers/todoListController');
+
+  // todoList Routes
+  app.route('/tasks')
+    .get(todoList.list_all_tasks)
+    .post(todoList.create_a_task);
+
+
+  app.route('/tasks/:taskId')
+    .get(todoList.read_a_task)
+    .put(todoList.update_a_task)
+    .delete(todoList.delete_a_task);
+};
